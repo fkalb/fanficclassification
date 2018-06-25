49a91f9292fe04585e9395451034de4656386bfa
fkalb <floflo91@gmx.de>
2018-06-26 01:27:17 +0200

    Added more novels and fixed bugs

--- prepareDownload.py
+++ prepareDownload.py
@@ -1,5 +1,5 @@
 """
-This script is used for downloading fanfiction from archiveofourown.com and saving those as .html files
+This script searches for fanfiction on archiveofourown.org and extracts the identifier of each found text and saves it to a txt file
 """
 #########
 #Imports#
@@ -9,38 +9,42 @@ import os
 import re
 import requests
 
-#########
-#Folders#
-#########
-work_dir = os.getcwd()
-id_dir = os.path.join(work_dir, "ID")
-
 ############
 #Parameters#
 ############
-base_url = "https://archiveofourown.org/works/"
+id_dir = os.path.join(os.getcwd(), "ID")
 search_url = "https://archiveofourown.org/works/search?"
-search_parameter = {"word_count": "&work_search[word_count]=5000-20000", "language": "&work_search[language_id]=1", "fandom_name": "&work_search[fandom_names]=", "page_num": "page="}
-movies = {"LOTR" : "The+Lord+of+the+Rings+-+J.+R.+R.+Tolkien", "HP" : "Harry+Potter+-+J.+K.+Rowling"}
-pages = 5       #   1 page = 20 results
+search_parameter = {
+     "word_count": "&work_search[word_count]=5000-20000"
+    ,"language": "&work_search[language_id]=1"
+    ,"fandom_name": "&work_search[fandom_names]="
+    ,"page_num": "page="
+    }
+novels = {
+ "GOT" : "Game+of+Thrones+%28TV%29"
+,"LOTR" : "The+Lord+of+the+Rings+-+J.+R.+R.+Tolkien"
+,"HP" : "Harry+Potter+-+J.+K.+Rowling"
+,"FB" : "Fantastic+Beasts+and+Where+to+Find+Them+%28Movies%29"
+,"PJ" : "Percy+Jackson+and+the+Olympians+%26+Related+Fandoms+-+All+Media+Types"
+,"TH" : "The+Hobbit+-+J.+R.+R.+Tolkien"
+}
+pages = 10       #   1 page = 20 results
+
 
 ###########
 #Functions#
 ###########
 
-def get_work_IDs(search_url, movie, result_pages):
+def get_work_IDs(search_url, search_parameter, novel, pages):
     
     ID_list = []
 
-    for page in range(1, result_pages+1): 
+    for page in range(1, pages+1): 
         
-        query_url = "".join((search_url, search_parameter["page_num"], str(page), search_parameter["word_count"], search_parameter["language"], search_parameter["fandom_name"], movie))
+        query_url = "".join((search_url, search_parameter["page_num"], str(page), search_parameter["word_count"], search_parameter["language"], search_parameter["fandom_name"], novel))
         
-        try:
-            result_page = requests.get(query_url)
-        except requests.exceptions.RequestException as e:
-            print("Error ", e,  "at: ", query_url)
-
+        result_page = requests.get(query_url)
+        
         resultpage_soup = BeautifulSoup(result_page.text, "lxml")
         IDs = resultpage_soup.select("h4.heading > a:nth-of-type(1)")
 
@@ -51,24 +55,29 @@ def get_work_IDs(search_url, movie, result_pages):
 
     return ID_list
 
-def write_word_IDs(IDs, movie):
-    with open(id_dir + "/" + movie + "_IDs.txt", "w", encoding="utf-8") as outfile:
+def write_word_IDs(IDs, novel):
+    with open(id_dir + "/" + novel + "_IDs.txt", "w", encoding="utf-8") as outfile:
         IDs_str = "\n".join(IDs)
         outfile.write(IDs_str)
 
-def main(id_dir, base_url, search_url, pages, movies):
+def main(id_dir, search_url, search_parameter, novels, pages):
     
     if not os.path.exists(id_dir):
         os.makedirs(id_dir)
     
-    for movie in movies:
-        IDs = get_work_IDs(search_url, movies[movie], pages)
-        print(len(IDs), "IDs found for", movie)
-
-        if (len(IDs) == pages * 20):
-            write_word_IDs(IDs, movie)
+    for novel in novels:
         
-        else:
-            print("Not enough IDs gathered for ", movie, "... Please restart this script!")
-    
-main(id_dir, base_url, search_url, pages, movies)
+        restart = True
+
+        while restart:
+            restart = False
+
+            IDs = get_work_IDs(search_url, search_parameter, novels[novel], pages)
+            print(len(IDs), "IDs found for", novel)
+            #print(IDs)
+            if (len(IDs) == pages * 20):
+                write_word_IDs(IDs, novel)
+            else:
+                print("Something went wrong. Not enough IDs gathered for: ", novel, ". Loop restarting!")
+                restart = True 
+main(id_dir, search_url, search_parameter, novels, pages)
